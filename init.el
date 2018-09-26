f506e030efeaf5107b1380430c3b1817cf432180
Rodrigo Koga <rkogaj@gmail.com>
2018-09-26 14:05:12 -0500

    Backup

--- init.el
+++ init.el
@@ -17,13 +17,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
- '(line-number-mode nil)
  '(package-selected-packages
    (quote
-    (which-key org-bullets magit ivy-yasnippet ivy company-ghc ghc smart-mode-line color-theme-sanityinc-tomorrow haskell-mode haskell-snippets projectile telephone-line yasnippet-snippets company yasnippet))))
+    (image+ dashboard rainbow-delimiters which-key org-bullets magit ivy-yasnippet ivy company-ghc ghc smart-mode-line color-theme-sanityinc-tomorrow haskell-mode haskell-snippets projectile telephone-line yasnippet-snippets company yasnippet))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
